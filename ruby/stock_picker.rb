619ae147fda5d48abe0d34e32168d631c79d204f
Salih Ozkan <it.salih.ozkan@gmail.com>
2020-11-09 22:24:28 +0100

    Cleaned up & finished the stock picker assignment

--- ruby/stock_picker.rb
+++ ruby/stock_picker.rb
@@ -1,54 +1,63 @@
-# store every pair that has a low value followed 
-# by a high value in a hash, and then in the end
-# iterate it, subtracting lowest value from
-# the highest value for each entry. Whichever
-# has the highest result wins
-
 def stock_picker(days_array)
 	results = Hash.new(0)
 	buy_amount = 0
-	buy_day = 0
 	sell_amount = 0
+	buy_day = 0
 	sell_day = 0
-	highest = 0
+	profit = 0
 	
-
+	# We're gonna loop through the entire array comparing the
+	# current (index) buy value to the rest of the values in
+	# the array beyond the current to see if they are higher
+	# if yes we store them and in the end make a comparison
+	# to see which pairs give the biggest difference (profit)
 	days_array.length.times do |index|
-		
 		buy_amount = days_array[index]
-		buy_day = index
-
 		sell_amount = days_array[index]
-		# puts "Loop #{(days_array.length - index)} times!"
+		buy_day = index
 		
+		# Check the array's values from current index to the last
+		# entry in the array and compare  
 		days_array[index..days_array.length].length.times do |index2|
 			next_index = index + index2
-			tmp_sell_amount = days_array[next_index]
+			next_sell_amount = days_array[next_index]
 			
-			# puts "Is #{tmp_sell_amount} > #{sell_amount}? (index: #{next_index})"
-			if tmp_sell_amount > sell_amount
-				# puts " -> Yes it is! (index: #{next_index})"
-				sell_amount = tmp_sell_amount
+			# If the amount in the next index is higher, set the
+			# appropriate vaiables (useless comment?)
+			if next_sell_amount > sell_amount
+				sell_amount = next_sell_amount
 				sell_day = next_index
 			end
 		end
 
-		# puts "Adding buying on day #{buy_day} and selling on #{sell_day} | Profit: #{sell_amount - buy_amount}" if buy_amount < sell_amount
+		# If we end up with a buy_amount that's lower than a
+		# sell_amount, add it to the hash and we'll make a 
+		# comparison in the end
 		results[buy_day] = sell_day if buy_amount < sell_amount
-		# puts "\n"
 	end
 
-	results.map do |key, value|
-		if (days_array[value] - days_array[key]) > highest
-			highest = (days_array[value] - days_array[key])
+	# If we have no results, then too bad
+	if !results.empty?
+		results.map do |key, value|
+			# Skip the iteration if the current entry is lower
+			# than the profit we already have
+			next if (days_array[value] - days_array[key]) <= profit
+
+			profit = (days_array[value] - days_array[key])
 			buy_day = key
 			sell_day = value
 		end
-	end
 
-	puts highest
-	puts results
-	puts "Buy on day ##{buy_day} and sell on day ##{sell_day} for a profit of $#{highest}"
+		# puts results
+		puts "Buy on day ##{buy_day} and sell on day ##{sell_day} for a profit of $#{profit}\n\n"
+	else 
+		puts "The given array contains no way to make profits.\n\n"
+	end
 end
 
 stock_picker([17, 3, 6, 9, 15, 8, 6, 1, 10])
+stock_picker([10, 9, 8, 7, 6, 5, 4, 3, 2, 1]) # Should be unprofitable
+
+# Results when ran:
+#	Buy on day #1 and sell on day #4 for a profit of $12
+#	The given array contains no way to make profits.
